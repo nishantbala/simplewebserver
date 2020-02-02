d915c603a66cc6918725c4c7414991cbea9447a6
Nishant <nishantbala@gmail.com>
2020-02-03 01:16:22 +0530

    Multithreaded execution and first test case

--- server/src/main/java/com/example/addition/server/ServerApplication.java
+++ server/src/main/java/com/example/addition/server/ServerApplication.java
@@ -12,4 +12,5 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+
 }
--- server/src/test/java/com/example/addition/server/ServerApplicationTests.java
+++ server/src/test/java/com/example/addition/server/ServerApplicationTests.java
@@ -1,13 +1,68 @@
 package com.example.addition.server;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class ServerApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+	@Autowired
+	private MockMvc mockMvc;
 
+	@Test
+	public void testEndRequest() throws Exception {
+		MvcResult mvcResult = mockMvc.perform(post("/").content("end"))
+                .andExpect(request().asyncStarted())
+                .andDo(MockMvcResultHandlers.log())
+                .andReturn();
+		this.mockMvc.perform(asyncDispatch(mvcResult))
+        .andExpect(status().isOk())
+        .andExpect(content().string("0"));
+	}
+	
+
+    public void testAdditionController() throws Exception 
+    {
+    	 List<String> listOfRequests = Arrays.asList("1", "2", "3", "end");
+    	 List<String> listOfResponses = Arrays.asList("6", "6", "6", "6");
+    	 List<MvcResult> mvcResultsList = new ArrayList<>();
+    	 Iterator<String> reqIterator = listOfRequests.iterator();
+    	 while(reqIterator.hasNext()) {
+    		 String request = reqIterator.next();
+    		 MvcResult mvcResult = mockMvc.perform(post("/").content(request))
+                     .andExpect(request().asyncStarted())
+                     .andDo(MockMvcResultHandlers.log())
+                     .andReturn();
+    		 mvcResultsList.add(mvcResult);
+    	 }
+         
+    	 for(int i=0; i < listOfRequests.size(); i++) {
+    		 MvcResult mvcResult  = mvcResultsList.get(i);
+    		 mockMvc.perform(asyncDispatch(mvcResult))
+             .andExpect(status().isOk())
+             .andExpect(content().string(listOfResponses.get(i)));
+    	 }
+          
+    }
 }
