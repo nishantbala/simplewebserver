fa821d458c4251a27777dacda247050a898d04c4
Nishant <nishantbala@gmail.com>
2020-02-04 01:04:42 +0530

    changes for maven

--- server/src/main/java/com/example/addition/server/ServerApplication.java
+++ server/src/main/java/com/example/addition/server/ServerApplication.java
@@ -2,15 +2,20 @@ package com.example.addition.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class ServerApplication {
+public class ServerApplication extends SpringBootServletInitializer {
+	  public static void main(String[] args) {
+	        SpringApplication.run(ServerApplication.class, args);
+	    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
-
+		@Override
+		protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+			return application.sources(ServerApplication.class);
+		}
 
 }
